f587cea28f271958c0fd98921bed2d6804d69eb0
Ruben Cancho <canchete@gmail.com>
2019-08-08 16:07:04 +0200

    Cleaning up

--- src/index.js
+++ src/index.js
@@ -1,28 +1,62 @@
 import { Machine, interpret } from 'xstate'
 
+// import { keyboardMachine } afrom './spanish-layout.js'
 // Stateless machine definition
 // machine.transition(...) is a pure function used by the interpreter.
-const toggleMachine = Machine({
+
+const machineConfig = {
   id: 'keyboard',
-  initial: 'init',
+  initial: 'noKey',
+  context: {
+    char: null
+  },
   states: {
-    init: { on: { press_dead: 'dead1' } },
-    dead1: { on: { press_a: 'char_á', release_dead: 'dead2' } },
-    dead2: { on: { press_a: 'char_á', press_dead: 'char_dead' } },
-    char_á: { on: { release_all: 'init' } },
-    char_dead: { on: { release_all: 'init' } }
+    noKey: {
+      on: { KeyA_DOWN: 'char_a' }, 
+      on: { Quote_DOWN: 'Quote_1' }
+    },
+    Quote_1: { 
+      on: { KeyA_DOWN: 'char_á' }, 
+      on: { Quote_UP: 'Quote_2' }
+    },
+    Quote_2: {
+      on: { KeyA_DOWN: 'char_á' },
+      on: { Quote_UP: 'char_Quote' }
+    },
+
+    // Character states
+    char_a:     { meta: { char: 'a' } },
+    char_á:     { meta: { char: 'á' } },
+    char_Quote: { meta: { char: '´' } }
   }
-});
+}
+
+const machineOptions = { actions: {}, activities: {}, guards: {}, services: {}}
+
+const layout = Machine(machineConfig, machineOptions);
 
 // Machine instance with internal state
-const toggleService = interpret(toggleMachine)
-  .onTransition(state => console.log(state.value))
+const keyboard = interpret(layout)
+  .onTransition(state => console.log(state.value + ":" + state.context.char))
   .start();
-// => 'inactive'
 
-toggleService.send('press_dead');
-// => 'active'
+const loadHandler = function(event) {
+  let bodytag = document.querySelector("body")
 
-toggleService.send('press_a');
-// => 'inactive':w
+  bodytag.onkeydown = keydownHandler
+  bodytag.onkeyup = keyupHandler
+}
 
+let keydownHandler = function (event) {
+  let event_name = event.code + '_DOWN'
+  console.log(event_name)
+  keyboard.send(event_name)
+}
+
+let keyupHandler = function (event) {
+  let event_name = event.code + '_UP'
+  console.log(event_name)
+  keyboard.send(event_name)
+}
+
+window.onload = loadHandler
